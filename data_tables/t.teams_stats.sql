842ef558895be7ec2b33385d2617142db153c6fd
Karych72 <petr.karol.96@mail.ru>
2023-01-06 22:23:32 +0100

    Fix team_stats

--- data_tables/t.teams_stats.sql
+++ data_tables/t.teams_stats.sql
@@ -1,14 +1,19 @@
 CREATE TABLE teams_stats(
-     team_id                         int,
+     team_id                        int,
+     games_played                   int,
      wins                           int,
      losses                         int,
      ot                             int,
-     pts                            int,
+     points                         int,
      procent_points                 DOUBLE PRECISION,
      goals_per_game                 DOUBLE PRECISION,
      goals_against_per_game         DOUBLE PRECISION,
      power_play_percentage          DOUBLE PRECISION,
      power_play_goals               int,
      power_play_goals_against       int,
+     power_play_opportunities       int,
+     penalty_kill_percentage        DOUBLE PRECISION,
+     shots_per_game                 DOUBLE PRECISION,
+     shots_allowed                  DOUBLE PRECISION,
      face_off_win_percentage        DOUBLE PRECISION
 );
